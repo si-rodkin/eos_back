5992bd93c8f67030dbf0fa3441095e1bbfcbb60b
rodkinsi <si.rodkin@yandex.ru>
2021-07-17 23:41:21 +0300

    Рефакторин кода строящего маршрут устройства.

--- src/main/kotlin/com/example/eyeofsauron/service/RouteBypassService.kt
+++ src/main/kotlin/com/example/eyeofsauron/service/RouteBypassService.kt
@@ -2,6 +2,7 @@ package com.example.eyeofsauron.service
 
 import com.example.eyeofsauron.entity.MarkerReader
 import com.example.eyeofsauron.entity.RouteBypass
+import com.example.eyeofsauron.exception.EntityNotFoundException
 import com.example.eyeofsauron.repository.RouteBypassRepository
 import org.springframework.data.jpa.domain.Specification
 import org.springframework.stereotype.Service
@@ -19,52 +20,54 @@ class RouteBypassService(private val repository: RouteBypassRepository) {
 
     fun getByRoute(routeId: Long) = repository.findByRouteId(routeId)
 
-    fun getByImeiAndLimit(imei: String, limitH: Long): List<RouteBypass> {
-        val now = LocalTime.now()
-        val limit = LocalTime.now().plusHours(limitH)
-        val nowWeekDay = LocalDateTime.now().dayOfWeek.value
-        val boundWeekDay = LocalDateTime.now().plusHours(limitH).dayOfWeek.value
+    /**
+     * Выборка обходов устройства с [imei] за ближайшие [limit] часов
+     */
+    fun getByImeiAndLimit(imei: String, limit: Long): List<RouteBypass> {
+        val nowWeekDay = LocalDateTime.now().dayOfWeek.value - 1
+        val boundWeekDay = LocalDateTime.now().plusHours(limit).dayOfWeek.value - 1
 
-        /** Если выборка затрагивает несколько дней, то */
+        // Если выборка затрагивает несколько дней, то
         if (nowWeekDay != boundWeekDay) {
             // 1. выбираем обходы, которые остались в этом дне
-            var spec = Specification.where(eqOrLaterThan(now).and(dayIs(nowWeekDay - 1)))
+            val spec = Specification.where(startTimeGte(LocalTime.now()).and(dayIs(nowWeekDay)))
+                // 2. выбираем обходы которые должны быть между днем запроса и днем лимита
+                .or(makeDayRange(nowWeekDay, (boundWeekDay - 1))
+                    .map { day -> dayIs(day) }
+                    .reduce { result, spec -> result.or(spec) }
+                )
+                // 3. выбираем обходы, которые будут в последнем дне с временем начала не позднее верхней границы выборки
+                .or(dayIs(boundWeekDay).and(startTimeLte(LocalTime.now().plusHours(limit))))
 
-            // 2. выбираем обходы которые должны быть между днем запроса и днем лимита
-            val dayRange = makeDayRange(nowWeekDay, (boundWeekDay - 2))
-            for (day in dayRange) {
-                spec = spec.or(dayIs(day))
+            return repository.findAll(deviceIs(imei).and(spec)).run {
+                // проход по номерам дней недели на которые выпадает выборка
+                makeDayRange(nowWeekDay, boundWeekDay)
+                    // выбираем обходы за данный день
+                    .map { day -> this.filter { it.day.contains(day.toString()) } }
+                    // сортируем обходы за данный день по времени начала
+                    .map { it.sortedBy { it.bypassTime } }
+                    // Собираем списки из предыдущих этапов в единый список
+                    .reduce { result, part -> result + part }
             }
-
-            // 3. выбираем обходы, которые будут в последнем дне с временем начала не позднее верхней границы выборки
-            spec = spec.or(dayIs(boundWeekDay - 1).and(eqOrEarlierThan(limit)))
-
-            val routeBypasses = repository.findAll(deviceIs(imei).and(spec))
-            val result = mutableMapOf<Int, List<RouteBypass>>()
-
-            for (day in makeDayRange(nowWeekDay - 1, boundWeekDay - 1)) {
-                val localResult = mutableListOf<RouteBypass>()
-                for (bypass in routeBypasses)
-                    if (bypass.day.contains(day.toString()))
-                        localResult.add(bypass);
-                result.put(day, localResult)
-            }
-
-            val veryResult = mutableListOf<RouteBypass>()
-            result.map { it.value }.map { it.sortedBy { it.bypassTime } }.map { veryResult.addAll(it) }
-            return veryResult
-        } else {
-            // Ограничение на время по умолчанию: если выборка в пределах дня
-            val spec = Specification.where(deviceIs(imei).and(dayIs(nowWeekDay)).and(eqOrLaterThan(now)).and(eqOrEarlierThan(limit)))
-            return  repository.findAll(spec)
         }
+
+        // Если выборка в пределах дня
+        return repository.findAll(
+            Specification.where(
+                deviceIs(imei)
+                    .and(dayIs(nowWeekDay))
+                    // Ограничение на время по умолчанию
+                    .and(startTimeGte(LocalTime.now()))
+                    .and(startTimeLte(LocalTime.now().plusHours(limit)))
+            )
+        )
     }
 
     fun create(routeBypass: RouteBypass) = repository.save(routeBypass)
 
     fun update(routeBypass: RouteBypass) {
-        if(!repository.existsById(routeBypass.id))
-            throw(Exception("Record not found!"))
+        if (!repository.existsById(routeBypass.id))
+            throw EntityNotFoundException()
         repository.save(routeBypass)
     }
 
@@ -72,30 +75,41 @@ class RouteBypassService(private val repository: RouteBypassRepository) {
 
     fun deleteById(id: Long) = repository.deleteById(id)
 
+    /**
+     * Создать правильную последовательность номеров дней недели с начальным номеров [begin] и конечным [endP]
+     */
     private fun makeDayRange(begin: Int, endP: Int): List<Int> {
-        val result = mutableListOf<Int>()
-        val end = if (begin > endP) endP + 7 else endP;
-        for (i in begin..end) result.add(i % 7)
-        return result
+        val end = if (begin > endP) endP + 7 else endP
+        return (begin..end).map { it % 7 }
     }
 
     companion object {
+        /** Спецификация ограничения выборки по [imei] устройства */
         fun deviceIs(imei: String) =
             Specification<RouteBypass> { root, _, criteriaBuilder ->
                 criteriaBuilder.equal(root.get<MarkerReader>("markerReader").get<String>("imei"), imei)
             }
 
+        /** Спецификация ограничения выборки по дню обхода */
         fun dayIs(day: Int) =
             Specification<RouteBypass> { root, _, criteriaBuilder ->
                 criteriaBuilder.like(root.get("day"), "%$day%")
             }
 
-        fun eqOrLaterThan(limit: LocalTime) =
+        /**
+         * Спецификация ограничения выборки по времени начала обхода
+         * (время начала обхода больше или равно заданному [limit])
+         */
+        fun startTimeGte(limit: LocalTime) =
             Specification<RouteBypass> { root, _, criteriaBuilder ->
                 criteriaBuilder.greaterThanOrEqualTo(root.get("bypassTime"), limit)
             }
 
-        fun eqOrEarlierThan(limit: LocalTime) =
+        /**
+         * Спецификация ограничения выборки по времени начала обхода
+         * (время начала обхода меньше или равно заданному [limit])
+         */
+        fun startTimeLte(limit: LocalTime) =
             Specification<RouteBypass> { root, _, criteriaBuilder ->
                 criteriaBuilder.lessThanOrEqualTo(root.get("bypassTime"), limit)
             }
